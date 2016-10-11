859d80e4af7868d4b2b1b5fbc8e795e203b1c70e
axeltoushirou <kmcid@mail.ru>
2016-10-11 23:45:38 +0800

    facebook auth

--- app/routes/index.js
+++ app/routes/index.js
@@ -45,6 +45,25 @@ router.post('/login', passport.authenticate('local-login', {
     failureFlash : true // allow flash messages
 }));
 
+// =====================================
+// FACEBOOK ROUTES =====================
+// =====================================
+// route for facebook authentication and login
+router.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+
+// handle the callback after facebook has authenticated the user
+router.get('/auth/facebook/callback',
+           passport.authenticate('facebook', {
+    successRedirect : '/profile',
+    failureRedirect : '/'
+}));
+
+// route for logging out
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
 function isLoggedIn(req, res, next) {
 
     // if user is authenticated in the session, carry on 
